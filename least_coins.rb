0dc641ec8f254d02c256d670214cca5121044b82
christina15339 <clamberg333@gmail.com>
2018-07-25 21:27:06 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,23 @@
-#write out your code here
-
 def least_coins(cents)
+  solution = {}
+  
+  num_quarters = cents/25
+  solution[:quarters]=num_quarters
+  cents = cents - 25*num_quarters 
+  
+  num_dimes = cents/10 
+  solution[:dimes] = num_dimes
+  cents = cents - 10*num_dimes
+  
+  num_nickles = cents/5 
+  solution [:nickles] = num_nickles 
+  cents = cents - 5*num_nickles
+  
+  num_pennys = cents/1 
+  solution[:pennys] = num_pennys
+  cents = cents - 1*num_pennys
+  puts solution
+  
+end 
+least_coins(1000)
 
-#Code your answer here!
-
-end
